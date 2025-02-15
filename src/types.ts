3887c70d1159baaece417c3ab55df53d04120f80
ricky-kiva <ricky.kiva@gmail.com>
2025-02-15 13:58:31 +0700

    feat(types): add Type Guard func. to validate `Diary` & String

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,16 @@ export interface Diary {
 }
 
 export type NewDiary = Omit<Diary, 'id'>;
+
+export const isDiary = (data: unknown): data is Diary => {
+  return typeof data === "object"
+    && data !== null
+    && "id" in data
+    && "date" in data
+    && "weather" in data
+    && "visibility" in data;
+};
+
+export const isString = (data: unknown): data is string => {
+  return typeof data === "string" || data instanceof String;
+};

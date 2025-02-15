38d431449212c9e4770b618cba91c47a862d5a84
ricky-kiva <ricky.kiva@gmail.com>
2025-02-15 14:04:33 +0700

    feat(service): handle POST `Diary` err.

--- src/services/diaryService.ts
+++ src/services/diaryService.ts
@@ -9,8 +9,30 @@ export const getAllDiaries = () => {
     .then(res => res.data);
 }
 
-export const createDiary = (object: NewDiary) => {
-  return axios
-    .post<Diary>(baseUrl, object)
-    .then(res => res.data);
+export const createDiary = async (object: NewDiary) => {
+  try {
+    return await axios
+      .post<Diary>(baseUrl, object)
+      .then(res => res.data);
+  } catch (e: unknown) {
+    if (axios.isAxiosError(e)) {
+      const errorObject = e.response?.data?.error;
+
+      if (Array.isArray(errorObject) && errorObject.length > 0) {
+        const firstError = errorObject[0];
+
+        if (firstError?.code && firstError?.path) {
+          return firstError.path.length > 0
+            ? firstError.received
+              ? `Incorrect ${firstError.path[0]}: ${firstError.received}`
+              : firstError.path[0]
+            : "A field is in an invalid format";
+        }
+      }
+
+      return "An unexpected HTTP error occured";
+    }
+
+    if (e instanceof Error) return e.message;
+  }
 }

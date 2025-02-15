a25b0a3961fda6a66485aa98188bbfd624dabc5c
ricky-kiva <ricky.kiva@gmail.com>
2025-02-16 01:17:27 +0700

    feat(types): add `Visibility` & `Weather` types

--- src/types.ts
+++ src/types.ts
@@ -8,6 +8,9 @@ export interface Diary {
 
 export type NewDiary = Omit<Diary, 'id'>;
 
+export type Visibility = 'great' | 'good' | 'ok' | 'poor';
+export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
+
 export const isDiary = (data: unknown): data is Diary => {
   return typeof data === "object"
     && data !== null

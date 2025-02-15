f550f578c227ab7f8624ef9c86466e25fc8092ec
ricky-kiva <ricky.kiva@gmail.com>
2025-02-15 11:56:44 +0700

    feat(types): add `comment` field to `Diary` interf.

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,8 @@ export interface Diary {
   id: number,
   date: string,
   weather: string,
-  visibility: string
+  visibility: string,
+  comment?: string
 }
 
 export type NewDiary = Omit<Diary, 'id'>;

aa6633cd3b4eb098da16011abe17d71a479065c4
ricky-kiva <ricky.kiva@gmail.com>
2025-02-15 11:57:09 +0700

    feat(src): impl. comp. to add new `Diary`

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
-import { Diary } from "./types";
-import { getAllDiaries } from "./services/diaryService";
+import { Diary, NewDiary } from "./types";
+import { createDiary, getAllDiaries } from "./services/diaryService";
 
-interface DiariesProps {
-  diaries: Diary[];
+type DiaryItemProps = Omit<Diary, 'comment'>;
+
+interface DiaryEntriesProps {
+  diaries: DiaryItemProps[];
 }
 
-const DiaryItem = (props: Diary) => {
+interface AddNewDiaryProps {
+  addDiary: (diary: Diary) => void
+}
+
+const DiaryItem = (props: DiaryItemProps) => {
   return (
     <div>
       <h3>{props.date}</h3>
@@ -18,9 +24,10 @@ const DiaryItem = (props: Diary) => {
   )
 }
 
-const Diaries = (props: DiariesProps) => {
+const DiaryEntries = (props: DiaryEntriesProps) => {
   return (
     <div>
+      <h3>Diary entries</h3>
       {props.diaries.map((d) =>
         <DiaryItem key={d.id}
           id={d.id}
@@ -29,6 +36,69 @@ const Diaries = (props: DiariesProps) => {
           weather={d.weather}
         />
       )}
+    </div>
+  )
+}
+
+const AddNewDiary = (props: AddNewDiaryProps) => {
+  const [date, setDate] = useState('');
+  const [visibility, setVisibility] = useState('');
+  const [weather, setWeather] = useState('');
+  const [comment, setComment] = useState('');
+
+  const diaryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    const newDiary: NewDiary = {
+      date: date,
+      visibility: visibility,
+      weather: weather,
+      comment: comment
+    }
+
+    createDiary(newDiary).then(data => {
+      props.addDiary(data);
+    });
+  }
+
+  return (
+    <div onSubmit={diaryCreation}>
+      <h3>Add new entry</h3>
+      <form>
+        <label htmlFor="date">date</label>
+        <input
+          type="text"
+          id="date"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+        />
+        <br/>
+        <label htmlFor="visibility">visibility</label>
+        <input
+          type="text"
+          id="visibility"
+          value={visibility}
+          onChange={(event) => setVisibility(event.target.value)}
+        />
+        <br/>
+        <label htmlFor="weather">weather</label>
+        <input
+          type="text"
+          id="weather"
+          value={weather}
+          onChange={(event) => setWeather(event.target.value)}
+        />
+        <br/>
+        <label htmlFor="comment">comment</label>
+        <input
+          type="text"
+          id="comment"
+          value={comment}
+          onChange={(event) => setComment(event.target.value)}
+        />
+        <br/>
+        <button type="submit">add</button>
+      </form>
     </div>
   )
 }
@@ -42,10 +112,14 @@ const App = () => {
     });
   }, []);
 
+  const addDiary = (diary: Diary) => {
+    setDiaries(diaries.concat(diary));
+  }
+
   return (
     <div>
-      <h2>Diary Entries</h2>
-      <Diaries diaries={diaries} />
+      <AddNewDiary addDiary={addDiary}/>
+      <DiaryEntries diaries={diaries} />
     </div>
   );
 }
